c6f4b2943dfae2e610aef69b526a4dcd11c6b804
AnkithaN142 <74245638+AnkithaN142@users.noreply.github.com>
2022-12-22 09:25:58 +0530

    new c file created

---
+++ swap.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,temp;
+   a=10;
+   b=20;
+   printf("Before swapping a and b : %d %d',a,b);
+   temp=a;
+   a=b;
+   b=temp;
+   printf("After swapping a and b : %d %d',a,b);
+  }
